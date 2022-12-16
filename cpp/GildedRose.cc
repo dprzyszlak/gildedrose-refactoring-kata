e262541dc1e9443365588060c2e135b8b3e40bc6
dprzyszlak <danielprzyszlakdesouky@yahoo.com>
2022-12-16 17:09:08 +0100

    Better ifs conditions

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -10,26 +10,24 @@ void GildedRose::updateQuality()
 {
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-            if (items[i].quality > 0)
-                if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                    items[i].quality--;
-        else
-            if (items[i].quality < 50)
+        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" &&
+        items[i].quality > 0 && items[i].name != "Sulfuras, Hand of Ragnaros")
+            items[i].quality--;
+        else if (items[i].quality < 50)
+        {
+            items[i].quality++;
+
+            if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
             {
-                items[i].quality++;
+                if (items[i].sellIn < 11)
+                    if (items[i].quality < 50)
+                        items[i].quality++;
 
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (items[i].sellIn < 11)
-                        if (items[i].quality < 50)
-                            items[i].quality++;
-
-                    if (items[i].sellIn < 6)
-                        if (items[i].quality < 50)
-                            items[i].quality++;
-                }
+                if (items[i].sellIn < 6)
+                    if (items[i].quality < 50)
+                        items[i].quality++;
             }
+        }
         
         if (items[i].name != "Sulfuras, Hand of Ragnaros")
             items[i].sellIn--;
